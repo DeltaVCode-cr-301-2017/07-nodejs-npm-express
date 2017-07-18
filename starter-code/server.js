fc426716ed39a81f5248f5ab18bf239e4dd90aec
Dustin <dyelland@gmail.com>
2017-07-18 13:06:08 -0500

    repushing

--- starter-code/server.js
+++ starter-code/server.js
@@ -15,8 +15,6 @@ const PORT = process.env.PORT || 3000;
 // TODO: Include all of the static resources as an argument to app.use()
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
-
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
