0ca040cc656463b229c795dbe8da7bdfb24582b5
Meepedy <david@psyon.org>
2017-07-18 12:13:59 -0500

    file cleanup

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 'use strict';
 
-// DONE: Initialize your project using NPM to create and populate a package.json file
 const express = require('express');
-// DONE: Require the Express package that you installed via NPM, and instantiate the app
-// Remember to install express, and be sure that it's been added to your package.json as a dependency
-// There is also a package here called body-parser, which is required in for use in a new route.
-// Be sure to install that and save it as a dependency after you create your package.json.
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('./starter-code/public'));
-// DONE: Include all of the static resources as an argument to app.use()
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 app.get('/new', function(request, response){
   response.sendFile('starter-code/public/new.html', {root: '.'});
 });
@@ -22,14 +15,10 @@ app.get('*', function(request, response){
 });
 
 app.post('/articles', bodyParser, function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html,
-  // and log that form data to the console. We will wire this up soon to actually
-  // write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
-})
+});
 
 app.listen(PORT, function() {
   console.log('express is listening on port ' + PORT);
-  // DONE: Log to the console a message that lets you know which port your server has started on
 });
